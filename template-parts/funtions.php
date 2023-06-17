59726789417d7d157aa5027be38756bfb92ed662
MULTISERVICIOS ARI <multiservicios.ari@hotmail.com>
2023-06-17 00:37:36 -0500

    q

--- template-parts/funtions.php
+++ template-parts/funtions.php
@@ -1,26 +1,27 @@
 <?php 
-function verificar_numero_tickets() {
-    $numero_ticket = $_POST['billing_cotasescolhidas'];
+function verificar_numero_tickets( $posted_data ) {
+    $numero_ticket = $posted_data['billing_cotasescolhidas'];
 
     if ( ! empty( $numero_ticket ) ) {
         global $wpdb;
 
         // Verificar si el número de ticket ya existe en otro pedido
         $existing_ticket = $wpdb->get_var( $wpdb->prepare( "
-            SELECT meta_value FROM $wpdb->woocommerce_order_itemmeta
+            SELECT COUNT(*) FROM $wpdb->woocommerce_order_itemmeta
             WHERE meta_key = 'billing_cotasescolhidas' AND meta_value = %s
         ", $numero_ticket ) );
 
-        if ( $existing_ticket ) {
+        if ( $existing_ticket > 0 ) {
             wc_add_notice( 'El número ' . $numero_ticket . ' no está disponible. Por favor, elige otro número para completar el proceso de pago.', 'error' );
         }
     }
 }
-add_action( 'woocommerce_checkout_process', 'verificar_numero_tickets' );
+add_action( 'woocommerce_after_checkout_validation', 'verificar_numero_tickets', 10, 1 );
+
 
 function agregar_texto_actualizacion_checkout() {
     // Coloca aquí el texto que deseas mostrar
-    $texto_actualizacion = 'VERCION 1';
+    $texto_actualizacion = 'VERCION q';
 
     echo '<p>' . $texto_actualizacion . '</p>';
 }
