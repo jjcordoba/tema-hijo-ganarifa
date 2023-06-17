9e6a4ee094fcaff62b9dead100a01e08dedef466
MULTISERVICIOS ARI <multiservicios.ari@hotmail.com>
2023-06-17 13:21:29 -0500

    1

--- functions.php
+++ functions.php
@@ -3,73 +3,7 @@
 include_once 'template-parts/funtions.php';
 include_once 'peronalizacion/login.php';
 
-add_filter( 'woocommerce_account_menu_items', 'ayudawp_ocultar_direccion', 999 );
 
- 
-
-function ayudawp_ocultar_direccion( $items ) {
-
-unset($items['downloads']);
-
-return $items;
-
-}
-
-
-
-/* Cancelar automaticamente pedidos pendientes tras 1 hora */
-
-add_action( 'woocommerce_order_status_pending', 'cancelped_cancelar_pedidos_pendientes' );
-
-function cancelped_cancelar_pedidos_pendientes( $order_id ) {
-
-if ( ! wp_next_scheduled( 'cancelped_cancelar_pedidos_pendientes_una_hora', array( $order_id ) ) ) {
-
-wp_schedule_single_event( time() + 3600, 'cancelped_cancelar_pedidos_pendientes_una_hora', array( $order_id ) );
-
-}
-
-}
-
-add_action( 'cancelped_cancelar_pedidos_pendientes_una_hora', 'bbloomer_cancel_order' );
-
-function bbloomer_cancel_order( $order_id ) {
-
-$order = wc_get_order( $order_id );
-
-wp_clear_scheduled_hook( 'cancelped_cancelar_pedidos_pendientes_una_hora', array( $order_id ) );
-
-if ( $order->has_status( array( 'pending' ) ) ) { 
-
-$order->update_status( 'cancelled', 'Pedido pendiente cancelado tras 1 hora' );
-
-}
-
-}
-
-/* Distinto boton de pago segun pasarela elegida */
-
-add_filter( 'woocommerce_available_payment_gateways', 'ayudawp_renombrar_boton_pago' );
-
-function ayudawp_renombrar_boton_pago( $gateways ) {
-
-if ( $gateways['cod'] ) {
-
-$gateways['cod']->order_button_text = 'Confirmar contra reembolso';
-
-} elseif ( $gateways['bacs'] ) {
-
-$gateways['bacs']->order_button_text = 'Ver datos bancarios';
-
-} elseif ( $gateways['stripe'] ) {
-
-$gateways['stripe']->order_button_text = 'Pago Seguro';
-
-} 
-
-return $gateways;
-
-}
 
 add_action('woocommerce_after_checkout_validation', 'verificar_tickets_repetidos', 10, 2);
 
