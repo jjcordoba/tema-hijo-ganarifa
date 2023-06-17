4981d664e569dad43bdb64bd5dd9482cacffcd56
MULTISERVICIOS ARI <multiservicios.ari@hotmail.com>
2023-06-17 00:40:40 -0500

    a

--- template-parts/funtions.php
+++ template-parts/funtions.php
@@ -1,6 +1,6 @@
 <?php 
-function verificar_numero_tickets( $posted_data ) {
-    $numero_ticket = $posted_data['billing_cotasescolhidas'];
+function verificar_numero_tickets( $passed, $product_id, $quantity ) {
+    $numero_ticket = $_POST['billing_cotasescolhidas'];
 
     if ( ! empty( $numero_ticket ) ) {
         global $wpdb;
@@ -13,15 +13,40 @@ function verificar_numero_tickets( $posted_data ) {
 
         if ( $existing_ticket > 0 ) {
             wc_add_notice( 'El número ' . $numero_ticket . ' no está disponible. Por favor, elige otro número para completar el proceso de pago.', 'error' );
+            $passed = false;
+        }
+    }
+
+    return $passed;
+}
+add_filter( 'woocommerce_add_to_cart_validation', 'verificar_numero_tickets', 10, 3 );
+
+function verificar_numero_tickets_checkout() {
+    $numero_ticket = $_POST['billing_cotasescolhidas'];
+
+    if ( ! empty( $numero_ticket ) ) {
+        global $wpdb;
+
+        // Verificar si el número de ticket ya existe en otro pedido
+        $existing_ticket = $wpdb->get_var( $wpdb->prepare( "
+            SELECT COUNT(*) FROM $wpdb->woocommerce_order_itemmeta
+            WHERE meta_key = 'billing_cotasescolhidas' AND meta_value = %s
+        ", $numero_ticket ) );
+
+        if ( $existing_ticket > 0 ) {
+            wc_add_notice( 'El número ' . $numero_ticket . ' no está disponible. Por favor, elige otro número para completar el proceso de pago.', 'error' );
+            wc_clear_notices();
+            return;
         }
     }
 }
-add_action( 'woocommerce_after_checkout_validation', 'verificar_numero_tickets', 10, 1 );
+add_action( 'woocommerce_checkout_process', 'verificar_numero_tickets_checkout' );
+
 
 
 function agregar_texto_actualizacion_checkout() {
     // Coloca aquí el texto que deseas mostrar
-    $texto_actualizacion = 'VERCION q';
+    $texto_actualizacion = 'VERCION a';
 
     echo '<p>' . $texto_actualizacion . '</p>';
 }
